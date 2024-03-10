5ca4349e767a4e70548af79af0d7ac60e23f8bcc
camilagsoares <camila.soares19@hotmail.com>
2024-03-10 10:54:24 -0300

    changes files

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+      customGreen: '#304F4E',
+      customColor: '#122F5D',
+      customWhite: '#ebebeb',
+      textVerde: "#88BB76",
+      bgFooter: "#041837"
+      // customGreenText: '#88BB76'
+      
+    }}
+  },
+  plugins: [],
+}
