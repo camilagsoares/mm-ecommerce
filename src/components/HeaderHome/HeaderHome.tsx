3af06dcb3352c6d7bb560edbc870c3555f09707b
camilagsoares <camila.soares19@hotmail.com>
2024-04-19 12:00:34 -0300

    .

--- src/components/HeaderHome/HeaderHome.tsx
+++ src/components/HeaderHome/HeaderHome.tsx
@@ -1,81 +1,105 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FiMenu } from 'react-icons/fi';
+import logo from '../../assets/logo.png';
+import backgroundImage from '../../assets/imagem.png';
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  padding: 20px; 
-  background: linear-gradient(to right, #13305E, #31504E);
+  padding: 10px;
+  background: linear-gradient(to bottom right, #051b3b, #234b8d 50%, #7cac71 100%);
   box-shadow: -2px 2px 4px rgba(0, 0, 0, 0.1);
   height: auto; 
-
-  @media (min-width: 769px) {
-    height: 800px;
-    flex-direction: row; 
-    align-items: center;
-    padding: 70px; 
+  
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: stretch;
+    height: auto; 
   }
+`;
+
+
+
+
+const MenuWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center; /* Alinha os elementos verticalmente */
+  width: 100%;
+  padding: 20px 90px; /* Ajuste o espaçamento conforme necessário */
+  margin-top: 20px; /* Ajuste para descer toda a linha */
+`;
+
+const Logo = styled.img`
+  height: 50px; /* Ajuste conforme necessário */
+`;
+
+const Icon = styled(FiMenu)`
+  font-size: 24px; /* Ajuste o tamanho do ícone conforme necessário */
+  color: #fff;
+  cursor: pointer;
 `;
 
 const Menu = styled.nav`
-  display: none;
+  text-align: center;
+  background: transparent;
 
-  @media (min-width: 769px) {
-    display: block;
-    text-align: center;
-    margin-bottom: 20px;
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center; /* Alinha os itens verticalmente */
+  }
 
-    ul {
-      list-style: none;
-      padding: 0;
-      margin: 0;
-      display: flex;
-      justify-content: center;
+  li {
+    margin: 0 20px; /* Ajuste as margens conforme necessário */
+    color: #fff;
+    font-weight: 500;
+    cursor: pointer;
+    transition: color 0.3s ease;
 
-      li {
-        margin: 0 10px;
-        color: #fff;
-        font-weight: 500;
-        cursor: pointer;
-        transition: color 0.3s ease;
-
-        &:hover {
-          color: #88bb76;
-        }
-      }
+    &:hover {
+      color: #88BB76;
     }
   }
 `;
+
 
 const TextContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   text-align: center;
+  width: 100%; 
+  margin-top: 20px; 
 
   @media (min-width: 769px) {
-    text-align: left;
+    text-align: left; 
+    width: 500px; 
     margin-right: 20px; 
   }
 `;
 
 const Title = styled.h1`
   color: #fff;
-  font-size: 24px;
+  font-size: 67px;
   margin-bottom: 10px;
   font-family: 'Inter';
   text-transform: uppercase;
   font-weight: 700;
 
-  @media (min-width: 769px) {
-    font-size: 67px;
+  @media (max-width: 768px) {
+    font-size: 50px; 
   }
 `;
 
 const GreenText = styled.span`
-  font-weight: 700;
+  font-weight: 900;
   color: #88BB76;
 `;
 
@@ -91,36 +115,71 @@ const Subtitle = styled.p`
   }
 `;
 
-const Logo = styled.img`
-  width: 100%;
-  max-width: 300px; 
-  height: auto; 
-  margin-bottom: 20px;
+const Image = styled.img`
+  width: 880px;
+  height: auto;
+  margin-top: 20px; /* Ajuste o valor conforme necessário */
 
-  @media (min-width: 769px) {
-    margin-bottom: 0; 
-    max-width: none; 
-    width: auto; 
+  @media (max-width: 768px) {
+    margin-top: 40px; /* Ajuste o valor conforme necessário para dispositivos móveis */
   }
 `;
 
+const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between; /* Adicionado para afastar os elementos */
+  width: 100%;
+  margin-top: 40px; /* Ajuste o valor conforme necessário */
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+    margin-top: 20px; /* Ajuste o valor conforme necessário para dispositivos móveis */
+  }
+`;
+
+
+const CentralizedContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`;
 const Header = () => {
   return (
     <Container>
-      {/* <Menu>
-        <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/">Produtos</a></li>
-          <li><a href='/sobre-nos'>Sobre Nós</a></li>
-        </ul>
-      </Menu> */}
-      <TextContainer>
-        <Title>
-          <GreenText>Soluções sustentáveis</GreenText> em energia solar.
-        </Title>
-        <Subtitle>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when </Subtitle>
-      </TextContainer>
-      <Logo src="https://i.ibb.co/MNHW2R7/Agrupar-1-copiar-2.png" alt="Logo" />
+      <MenuWrapper>
+        <Logo src={logo} alt="Logo" />
+        <Menu>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/">Produtos</a>
+            </li>
+            <li>
+              <a href='/sobre-nos'>Sobre Nós</a>
+            </li>
+          </ul>
+        </Menu>
+        <Icon />
+
+      </MenuWrapper>
+      <ContentContainer>
+        <CentralizedContainer>
+          <TextContainer>
+            <Title>
+              <GreenText>Soluções sustentáveis</GreenText> em energia solar.
+            </Title>
+            <Subtitle><b>Deixe o sol pagar sua conta de energia!</b> Solicite um orçamento</Subtitle>
+          </TextContainer>
+        </CentralizedContainer>
+        <Image src={backgroundImage} alt="Background" />
+      </ContentContainer>
     </Container>
   );
 };
--- src/pages/Index/style.js
+++ src/pages/Index/style.js
@@ -1,129 +1,125 @@
 import styled from "styled-components";
 
 export const AlignCenter = styled.div`
-display: flex;
-align-items: center;
-justify-content: center;
-`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 export const CircleGreen = styled.div`
-width: 25px;
-height: 24px;
-border-radius: 4px;
-margin-top: 80px;
-background-color: #88BB76;
+  width: 25px;
+  height: 24px;
+  border-radius: 4px;
+  margin-top: 80px;
+  background-color: #88BB76;
 
-
-@media (max-width: 768px) {
+  @media (max-width: 768px) {
     width: 17px;
     height: 17px;
     margin-top: 40px;
   }
-`
-
+`;
 
 export const Container = styled.div`
-display: flex;
-align-items: center;
-justify-content: center;
-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
 
-@media (max-width: 768px) {
-  flex-direction: column;
-}
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
 `;
-
 
 export const Card = styled.div`
-background-color: #fff;
-border: 2px solid #00ab6b;
-border-radius: 0.5rem;
-padding: 1rem;
-display: flex;
+  background-color: #fff;
+  border: 2px solid #00ab6b;
+  border-radius: 0.5rem;
+  padding: 1rem;
+  display: flex;
 
-@media (max-width: 768px) {
-  flex-direction: column;
-  align-items: center;
-}
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+  }
 `;
 
-
 export const Content = styled.div`
-flex: 1;
+  flex: 1;
 `;
 
 export const Title = styled.p`
-color: #1e3a8a;
-font-weight: bold;
-text-decoration: underline;
+  color: #1e3a8a;
+  font-weight: bold;
+  text-decoration: underline;
 `;
 
 export const Heading = styled.h1`
-font-family: 'Inter', sans-serif;
-font-weight: 800;
-font-size: 2.25rem;
-line-height: 2.5rem;
-letter-spacing: 0.05em;
+  font-family: 'Inter', sans-serif;
+  font-weight: 800;
+  font-size: 2.25rem;
+  line-height: 2.5rem;
+  letter-spacing: 0.05em;
 `;
 
 export const Text = styled.p`
-color: #555;
+  color: #555;
 `;
 
 export const Price = styled.p`
-font-weight: bold;
+  font-weight: bold;
 `;
 
 export const Button = styled.button`
-background-color: #0078d4;
-color: #fff;
-padding: 0.5rem 1rem;
-border-radius: 9999px;
-display: flex;
-align-items: center;
-justify-content: center;
-transition: background-color 0.3s;
-border: none;
-cursor: pointer;
+  background-color: #0078d4;
+  color: #fff;
+  padding: 0.5rem 1rem;
+  border-radius: 9999px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: background-color 0.3s;
+  border: none;
+  cursor: pointer;
 
-&:hover {
-  background-color: #005a9e;
-}
+  &:hover {
+    background-color: #005a9e;
+  }
 `;
 
 export const Image = styled.img`
-object-fit: cover;
-width: 100%;
-height: 100%;
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
 `;
 
 export const CardContainer = styled.div`
-margin-left: 1rem;
-flex-shrink: 0;
-overflow: hidden; 
+  margin-left: 1rem;
+  flex-shrink: 0;
+  overflow: hidden;
 `;
 
 export const AdditionalImagesContainer = styled.div`
-display: flex;
-margin-left: 1rem;
-flex-shrink: 0;
+  display: flex;
+  margin-left: 1rem;
+  flex-shrink: 0;
 
-@media (max-width: 768px) {
-  display: none; 
-}
+  @media (max-width: 768px) {
+    display: none;
+  }
 `;
 
 export const ContainerForm = styled.div`
-display: flex;
-justify-content: center;
-position: relative;
+  display: flex;
+  justify-content: center;
+  position: relative;
 `;
 
 export const BlueBackground = styled.div`
- position: relative;
+  position: relative;
   width: 100vw;
   border-radius: 30px;
-  max-width: 1600px; 
+  max-width: 1600px;
   height: 48vw;
   max-height: 30rem;
   background-color: #265094;
@@ -146,68 +142,83 @@ export const TextCardBlue = styled.div`
   }
 `;
 
-
 export const GreenFormContainer = styled.div`
-background-color: #88bb76;
-width: 100vw;
-max-width: 45rem; 
-padding: 2rem;
-border-radius: 30px;
-box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); 
-position: absolute; 
-top: -2rem; 
-right: 4rem;
-margin-bottom: -2rem; 
+  background-color: #88bb76;
+  width: 100vw;
+  max-width: 45rem;
+  padding: 2rem;
+  border-radius: 30px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  position: absolute;
+  top: -2rem;
+  right: 4rem;
+  margin-bottom: -2rem;
 `;
 
-
 export const ResponsiveGreenFormContainer = styled(GreenFormContainer)`
+label {
+  color : white;
+  font-family: 'Inter';
+  font-style: bold;
+}
   @media (max-width: 768px) {
-    width: 90%; /* Ajuste a largura */
+    width: 90%;
     max-width: none;
     right: auto;
     left: 50%;
     transform: translateX(-50%);
-    top: 1rem; /* Ajuste a margem superior */
-    margin-bottom: 1rem; /* Ajuste a margem inferior */
+    top: 1rem;
+    margin-bottom: 1rem;
   }
 `;
 
 export const StyledInput = styled.input`
-border: 1px solid #cbd5e0; 
-border-radius: 1rem;
-padding: 0.75rem;
-width: calc(100% - 2 * 0.75rem); 
-margin-bottom: 1rem;
-outline: none;
+  border: 1px solid #cbd5e0;
+  border-radius: 1rem;
+  padding: 0.5rem;
+  width: calc(100% - 2 * 0.5rem);
+  margin-bottom: 1rem;
+  outline: none;
+  font-size: 14px; /* Tamanho da fonte ajustável para dispositivos móveis */
 `;
 
 export const StyledTextArea = styled.textarea`
-border: 1px solid #cbd5e0; 
-border-radius: 1rem; 
-padding: 0.75rem;
-width: calc(100% - 2 * 0.75rem);
-margin-bottom: 1rem;
-resize: none;
-outline: none;
+  border: 1px solid #cbd5e0;
+  border-radius: 1rem;
+  padding: 0.5rem;
+  width: calc(100% - 2 * 0.5rem);
+  margin-bottom: 1rem;
+  resize: none;
+  outline: none;
+  font-size: 14px; /* Tamanho da fonte ajustável para dispositivos móveis */
 `;
 
-
 export const SubmitButton = styled.button`
-background-color: #265094;
-color: white;
-padding: 1rem 5rem;
-border-radius: 9999px;
-cursor: pointer;
-transition: background-color 0.3s ease;
-&:hover {
-  background-color: #1a2953; 
-}
+  background-color: #265094;
+  color: white;
+  padding: 0.75rem 3rem; /* Ajuste o padding conforme necessário */
+  border-radius: 9999px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #1a2953;
+  }
 `;
 
 export const ContainerFormBlue = styled.div`
-  margin-top: 180px; /* Reduza a margem superior */
+  margin-top: 180px;
   margin-bottom: 200px;
-`
+`;
 
-export const StyledSelect = styled.select``
+export const StyledSelect = styled.select`
+  border: 1px solid #cbd5e0;
+  border-radius: 1rem;
+  padding: 0.5rem;
+  width: calc(100% - 2 * 0.5rem);
+  margin-bottom: 1rem;
+  outline: none;
+  font-size: 14px; /* Tamanho da fonte ajustável para dispositivos móveis */
+`;
+
+
