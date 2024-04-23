2ec9fc45744b389f4254a81d9d1d9e238079e533
camilagsoares <camila.soares19@hotmail.com>
2024-04-23 12:53:33 -0300

    fix footer.

--- src/pages/Index/components/Form/Form.tsx
+++ src/pages/Index/components/Form/Form.tsx
@@ -1,4 +1,4 @@
-import { ContainerForm, BlueBackground, ResponsiveGreenFormContainer, StyledInput, StyledTextArea, SubmitButton, TextCardBlue, ContainerFormBlue, StyledSelect, Title } from "../../style"
+import { ContainerForm, BlueBackground, ResponsiveGreenFormContainer, StyledInput, StyledTextArea, SubmitButton, TextCardBlue, ContainerFormBlue, StyledSelect, Title, BlueText, StyledRadio, StyledSelectWithMargin, FieldsContainer, Header, GreenLine } from "../../style"
 
 export function Form() {
 
@@ -10,28 +10,42 @@ export function Form() {
                         <form>
                             <div>
                                 <div>
-                                    <StyledSelect id="sobreImovel" name="sobreImovel">
-                                        <option value="">Selecione sobre o imóvel</option>
-                                        <option value="casa">Casa</option>
-                                        <option value="loja">Loja</option>
-                                        <option value="rural">Rural</option>
-                                        <option value="industrial">Industrial</option>
-                                        <option value="apartamento">Apartamento</option>
-                                        <option value="condominio">Condomínio</option>
-                                        <option value="outros">Outros</option>
-                                    </StyledSelect>
+                                    <span className="title-Radio">Tipo de imóvel:</span>
+                                    <div>
+                                        <StyledRadio type="radio" id="residencial" name="tipoImovel" value="residencial" />
+                                        <label htmlFor="residencial">Residencial</label>
+                                    </div>
+                                    <div>
+                                        <StyledRadio type="radio" id="comercial" name="tipoImovel" value="comercial" />
+                                        <label htmlFor="comercial">Comercial</label>
+                                    </div>
                                 </div>
-                                <div>
-                                    <StyledSelect id="tituloImovel" name="tituloImovel">
-                                        <option value="">Selecione o título do imóvel</option>
-                                        <option value="proprio">Próprio</option>
-                                        <option value="alugado">Alugado</option>
-                                        <option value="conjuge">Cônjuge</option>
-                                        <option value="pais">Pais</option>
-                                        <option value="filhos">Filhos</option>
-                                        <option value="outros">Outros</option>
-                                    </StyledSelect>
-                                </div>
+
+                                <FieldsContainer>
+                                    <div>
+                                        <StyledSelectWithMargin id="sobreImovel" name="sobreImovel">
+                                            <option value="">Selecione sobre o imóvel</option>
+                                            <option value="casa">Casa</option>
+                                            <option value="loja">Loja</option>
+                                            <option value="rural">Rural</option>
+                                            <option value="industrial">Industrial</option>
+                                            <option value="apartamento">Apartamento</option>
+                                            <option value="condominio">Condomínio</option>
+                                            <option value="outros">Outros</option>
+                                        </StyledSelectWithMargin>
+                                    </div>
+                                    <div>
+                                        <StyledSelect id="tituloImovel" name="tituloImovel">
+                                            <option value="">Selecione o título do imóvel</option>
+                                            <option value="proprio">Próprio</option>
+                                            <option value="alugado">Alugado</option>
+                                            <option value="conjuge">Cônjuge</option>
+                                            <option value="pais">Pais</option>
+                                            <option value="filhos">Filhos</option>
+                                            <option value="outros">Outros</option>
+                                        </StyledSelect>
+                                    </div>
+                                </FieldsContainer>
                             </div>
                             <div>
                                 <StyledInput type="text" id="ultimaFatura" name="ultimaFatura" placeholder="Última Fatura:" />
@@ -48,13 +62,7 @@ export function Form() {
                                 <label htmlFor="phone">Telefone:</label>
                                 <StyledInput type="text" id="phone" name="phone" />
                             </div>
-                            <div>
-                                <StyledSelect id="tipoImovel" name="tipoImovel">
-                                    <option value="">Selecione o tipo de imóvel</option>
-                                    <option value="residencial">Residencial</option>
-                                    <option value="comercial">Comercial</option>
-                                </StyledSelect>
-                            </div>
+
                             <div>
                                 <StyledTextArea id="message" rows={4} placeholder="Mensagem:" name="message" />
                             </div>
@@ -62,10 +70,13 @@ export function Form() {
                         </form>
                     </ResponsiveGreenFormContainer>
                     <TextCardBlue>
-                        <h2 style={{ color: 'white', margin: '0' }}>Solicite um orçamento</h2>
+                        <Header>Solicite um orçamento</Header>
+                        <GreenLine />
+
+                        {/* <h2 style={{ color: 'white', margin: '0' }}>Solicite um orçamento</h2> */}
                         <h1 style={{ margin: '0' }}>Como podemos ajudar em seu negócio?</h1>
                     </TextCardBlue>
-                    
+
                 </BlueBackground>
             </ContainerForm>
         </ContainerFormBlue>
