0c51d4f7142c91095a0ec50624d451580c5997ca
camilagsoares <camila.soares19@hotmail.com>
2024-05-07 11:51:46 -0300

    .

--- src/pages/DetailsProduct/DetailsProduct.tsx
+++ src/pages/DetailsProduct/DetailsProduct.tsx
@@ -1,33 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { ButtonAdd, ButtonComprar, Container, DescriptionParagraph, NormalPrice, Price, ProductImage, ProductInfo, StrikedPrice, QuantityContainer, QuantityButton } from "./styles";
+import { ButtonAdd, ButtonComprar, Container, DescriptionParagraph, NormalPrice, Price, ProductImage, ProductInfo, StrikedPrice, QuantityContainer, QuantityButton, SmallImage, ImageContainer, AdditionalInfo, SmallImagesContainer } from "./styles";
+import { Product, products } from "./products";
 
-interface Product {
-  id: number;
-  name: string;
-  description: string;
-  moreDescription: string
-  moreDescriptionTwo?: string
-  moreDescriptionThree?: string
-  price: string;
-  image: string;
-}
 
 export const DetailsProduct = () => {
   const { id } = useParams<{ id: string }>();
   const [product, setProduct] = useState<Product | null>(null);
   const [quantity, setQuantity] = useState<number>(1);
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
 
   useEffect(() => {
     const getProductDetails = async (productId: number) => {
-      const products: Product[] = [
-        { id: 1, name: 'MMS - STARTER HIGH', description: 'Produto de alta qualidade.', price: ' 45.000.00,35.000.00', image: 'https://static.wixstatic.com/media/e8f377_e79a2d5611354c41b13c4632c72dccdf~mv2.png/v1/fill/w_731,h_731,q_90/e8f377_e79a2d5611354c41b13c4632c72dccdf~mv2.webp' , moreDescription: 'Kit Bomba Solar Submersa 380V até 50m ou 75.950L/dia.', moreDescriptionTwo: 'Bomba solar de 14 estágios, 5CV com inversor externo.' },
-        { id: 2, name: 'MMS - STARTER MEDIUM', description: 'Produto de qualidade média.', price: '17.400.00,13.500.00', image: 'https://static.wixstatic.com/media/e8f377_e79a2d5611354c41b13c4632c72dccdf~mv2.png/v1/fill/w_731,h_731,q_90/e8f377_e79a2d5611354c41b13c4632c72dccdf~mv2.webp', moreDescription: 'Kit Bomba Solar Submersa 2200W até 153m ou 11.000L/dia' , moreDescriptionTwo: 'Bomba solar de 37 estágios, 3CV com controlador externo MMS Starter.'},
-        { id: 3, name: 'MMS - STARTER BASIC', description: 'Produto de qualidade básica.', price: '10.400.00,8.000.00', image: 'https://static.wixstatic.com/media/e8f377_e79a2d5611354c41b13c4632c72dccdf~mv2.png/v1/fill/w_731,h_731,q_90/e8f377_e79a2d5611354c41b13c4632c72dccdf~mv2.webp' , moreDescription: 'Kit Bomba Solar', moreDescriptionTwo: 'Submersa 1100W até 42m ou 23.000L/dia.' , moreDescriptionThree: 'Bomba solar de 13 estágios, 1CV com controlador externo MMS Starter.'},
-      ];
+
 
       const foundProduct = products.find(product => product.id === Number(productId));
       setProduct(foundProduct || null);
+      setSelectedImage(foundProduct?.image || null);
     };
 
     getProductDetails(Number(id));
@@ -45,15 +34,28 @@ export const DetailsProduct = () => {
     }
   };
 
+  const handleImageClick = (image: string) => {
+    setSelectedImage(image);
+  };
+
   if (!product) {
     return <div>Carregando...</div>;
   }
 
-  return ( 
+  return (
     <Container>
-      <ProductImage>
-        <img src={product.image} alt={product.name} />
-      </ProductImage>
+      <ImageContainer>
+        <SmallImagesContainer>
+          {product.images.map((image, index) => (
+            <SmallImage key={index} onClick={() => handleImageClick(image)}>
+              <img src={image} alt={`Thumbnail ${index}`} />
+            </SmallImage>
+          ))}
+        </SmallImagesContainer>
+        <ProductImage>
+          <img src={selectedImage ? selectedImage : undefined} alt={product.name} />
+        </ProductImage>
+      </ImageContainer>
       <ProductInfo>
         <h2>{product.name}</h2>
         <DescriptionParagraph>{product.description}</DescriptionParagraph>
@@ -75,5 +77,6 @@ export const DetailsProduct = () => {
         <ButtonComprar>Comprar</ButtonComprar>
       </ProductInfo>
     </Container>
+
   );
 };
